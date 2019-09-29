bc2e4fe1f244d1d3509148bd9629d88f27989422
Double-oxygeN <mail@double-oxygen.net>
2019-09-29 13:43:20 +0900

    use of main module is deprecated

--- src/twofish.nim
+++ src/twofish.nim
@@ -1,7 +1,12 @@
-# This is just an example to get you started. A typical library package
-# exports the main API in this file. Note that you cannot rename this file
-# but you can remove it if you wish.
+# Copyright (c) 2019 Double-oxygeN
+# 
+# This software is released under the MIT License.
+# https://opensource.org/licenses/MIT
 
-proc add*(x, y: int): int =
-  ## Adds two files together.
-  return x + y
+stderr.writeLine "\e[33m************************"
+stderr.writeLine "'import twofish' is deprecated."
+stderr.writeLine "Please use 'import twofish/twofish128', 'import twofish/twofish192' or 'import twofish/twofish256'."
+stderr.writeLine "************************\e[0m"
+
+import twofish/twofish128
+export twofish128
