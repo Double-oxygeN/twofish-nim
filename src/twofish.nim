7db4e3898006848138286ce5ca432f9a028c0135
Double-oxygeN <mail@double-oxygen.net>
2019-09-30 22:04:10 +0900

    use warning message instead of IO write

--- src/twofish.nim
+++ src/twofish.nim
@@ -3,10 +3,7 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-stderr.writeLine "\e[33m************************"
-stderr.writeLine "'import twofish' is deprecated."
-stderr.writeLine "Please use 'import twofish/twofish128', 'import twofish/twofish192' or 'import twofish/twofish256'."
-stderr.writeLine "************************\e[0m"
+{.warning: "'import twofish' is deprecated. Please use 'import twofish/twofish128', 'import twofish/twofish192' or 'import twofish/twofish256'.".}
 
 import twofish/twofish128
 export twofish128
