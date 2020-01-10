0f4344bc43cc44cb93d4a3b73131522ad0b8df19
Double-oxygeN <mail@double-oxygen.net>
2020-01-10 14:47:05 +0900

    fix bug for windows

--- src/twofish/csprng.nim
+++ src/twofish/csprng.nim
@@ -8,6 +8,11 @@
 import streams
 import twofish256
 
+when defined(windows):
+  {.emit: "/*INCLUDESECTION*/\p#define _CRT_RAND_S".}
+
+  proc randSecure(randomValue: ptr cuint): cint {.importc: "rand_s", cdecl, header: "<stdlib.h>".}
+
 type
   SecureRand* = object
     ctr: Block
@@ -15,11 +20,22 @@ type
     buf: seq[uint32]
 
 proc initSecureRand*(prng: var SecureRand) =
-  const urandom = "/dev/urandom"
-  var strm = openFileStream(urandom, fmRead, 16)
-  discard strm.readData(addr prng.ctr[0], 16)
-  discard strm.readData(addr prng.key[0], 32)
-  close strm
+  when defined(windows):
+    for ctrWord in prng.ctr.mitems:
+      if randSecure(addr ctrWord) != 0:
+        stderr.writeLine "Something is wrong with rand_s() function!"
+
+    for keyWord in prng.key.mitems:
+      if randSecure(addr keyWord) != 0:
+        stderr.writeLine "Something is wrong with rand_s() function!"
+
+  else:
+    const urandom = "/dev/urandom"
+    var strm = openFileStream(urandom, fmRead, 16)
+    discard strm.readData(addr prng.ctr[0], 16)
+    discard strm.readData(addr prng.key[0], 32)
+    close strm
+
   prng.buf = @[]
 
 proc getNum*(prng: var SecureRand): uint32 =
